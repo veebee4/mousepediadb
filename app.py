d2d35fbefcd96cb98baa39199a3c4328c53f0fa9
Varinia Butcher <varinia_b@hotmail.com>
2024-08-10 10:41:07 +0000

    Add app.py file

---
+++ app.py
@@ -0,0 +1,10 @@
+# import modules
+import os
+from mousepedia import app
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
