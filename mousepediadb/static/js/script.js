f1414949f76196a3cce134fe570f0bf3b1a630b6
Varinia Butcher <varinia_b@hotmail.com>
2024-08-10 12:19:41 +0000

    Add nav and footer to base.html

---
+++ mousepediadb/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // initialises the side nav bar
+    let sidenavbar = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenavbar);
+  });
