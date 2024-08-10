771aade15760c95c834c0ea37f35181898500cda
Varinia Butcher <varinia_b@hotmail.com>
2024-08-10 14:24:12 +0000

    Fix form on add_parks page not recognising data in input field

--- mousepediadb/routes.py
+++ mousepediadb/routes.py
@@ -1,5 +1,5 @@
 # import modules and functions
-from flask import render_template
+from flask import render_template, session, request, redirect, url_for
 from mousepediadb import app, db
 from mousepediadb.models import Park, Restaurant, Ride
 
@@ -16,4 +16,33 @@ def parks():
 
 @app.route("/add_park", methods=["GET", "POST"])
 def add_park():
+    if request.method == "POST":
+        park_name = request.form.get("park_name")
+        park_address = request.form.get("park_address")
+        park_description = request.form.get("park_description")
+        date_opened = request.form.get("date_opened")
+        time_open = request.form.get("time_open")  
+        time_closed = request.form.get("time_closed")  
+        num_restaurants = request.form.get("num_restaurants")
+        num_rides = request.form.get("num_rides")
+        entertainment = request.form.get("entertainment")
+        special_features = request.form.get("special_features")
+        transport_between_parks = request.form.get("transport_between_parks")
+        
+        park = Park(
+            park_name=park_name,
+            park_address=park_address,
+            park_description=park_description,
+            date_opened=date_opened,
+            time_open=time_open,
+            time_closed=time_closed,
+            num_restaurants=num_restaurants,
+            num_rides=num_rides,
+            entertainment=entertainment,
+            special_features=special_features,
+            transport_between_parks=transport_between_parks
+        )
+        db.session.add(park)
+        db.session.commit()
+        return redirect(url_for("parks"))
     return render_template("add_park.html")
--- mousepediadb/static/js/script.js
+++ mousepediadb/static/js/script.js
@@ -3,3 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenavbar = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenavbar);
   });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // initialises the date picker for date_opened on the add_park form 
+    let dateOpened = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(dateOpened);
+    });
+  
+document.addEventListener('DOMContentLoaded', function() {
+    // initialises the time picker for time_open & time_closed on the add_park form
+    let openingTimes = document.querySelectorAll('.timepicker');
+    M.Timepicker.init(openingTimes);
+  });
