c031500c3d122c62ed37182f159e8f9e3f50557a
Varinia Butcher <varinia_b@hotmail.com>
2024-08-10 12:41:02 +0000

    Add routes for parks and add_park pages

--- mousepediadb/routes.py
+++ mousepediadb/routes.py
@@ -7,3 +7,13 @@ from mousepediadb.models import Park, Restaurant, Ride
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/parks")
+def parks():
+        return render_template("parks.html")
+
+
+@app.route("/add_park", methods=["GET", "POST"])
+def add_park():
+    return render_template("add_park.html")
