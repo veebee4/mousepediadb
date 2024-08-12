789cab66cc22fe9e41ca68297bcc4f37506b136a
Varinia Butcher <varinia_b@hotmail.com>
2024-08-12 19:44:40 +0000

    Add more details and styling to parks cards

--- mousepediadb/static/js/script.js
+++ mousepediadb/static/js/script.js
@@ -7,11 +7,16 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
     // initialises the date picker for date_opened on the add_park form 
     let dateOpened = document.querySelectorAll('.datepicker');
-    M.Datepicker.init(dateOpened);
+    M.Datepicker.init(dateOpened, {
+      format: 'dd/mm/yyyy' // displaying date in UK format
     });
+  });
   
 document.addEventListener('DOMContentLoaded', function() {
     // initialises the time picker for time_open & time_closed on the add_park form
     let openingTimes = document.querySelectorAll('.timepicker');
-    M.Timepicker.init(openingTimes);
+    M.Timepicker.init(openingTimes, {
+      format: 'HH:mm', // Time format for 24-hour clock
+      twelveHour: true, // Set to true for 12-hour clock with AM/PM
+    });
   });
