119011fe5626b24791f9e08640d705d3a3258742
Varinia Butcher <varinia_b@hotmail.com>
2024-08-24 14:54:39 +0000

    Remove flask-login details from all files

--- mousepediadb/__init__.py
+++ mousepediadb/__init__.py
@@ -2,20 +2,21 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
 
 # checking if env.py file exists for environment settings
 if os.path.exists("env.py"):
     import env
 
+
 # creating flask application and app configuration files
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-login_manager = LoginManager()
-login_manager.init_app(app)
+
 
 # assigning SQLAlchemy class instance to variable & migrate to update models
 db = SQLAlchemy(app)
+
 
 from mousepediadb import routes
--- mousepediadb/models.py
+++ mousepediadb/models.py
@@ -1,7 +1,4 @@
 from mousepediadb import db
-from werkzeug.security import generate_password_hash
-from werkzeug.security import check_password_hash
-from flask_login import UserMixin
 
 class Restaurant(db.Model):
     # Schema for the Restaurants model
@@ -79,19 +76,3 @@ class Park(db.Model):
             f"  Special Features        : {self.special_features}\n"
             f"  Transport Between Parks : {self.transport_between_parks}\n"
         )
-
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), index=True, unique=True)
-    email = db.Column(db.String(50), index=True, unique=True)
-    password_hash = db.Column(db.String(15))
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def __repr__(self):
-        return '<User{}>'.format(self.username)
