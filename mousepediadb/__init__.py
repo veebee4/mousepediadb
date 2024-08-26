ed371a8b017b7a355075ef7ab4e1f7b1f2948c14
Varinia Butcher <varinia_b@hotmail.com>
2024-08-26 13:39:26 +0000

    Update init file for deployment

--- mousepediadb/__init__.py
+++ mousepediadb/__init__.py
@@ -12,7 +12,13 @@ if os.path.exists("env.py"):
 # creating flask application and app configuration files
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 
 # assigning SQLAlchemy class instance to variable & migrate to update models
